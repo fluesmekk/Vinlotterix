9f4d76b05feba7c717a2855388852a954593806a
GET Academy <terje@kolderup.net>
2019-05-08 13:18:45 +0200

    x

---
+++ v3/js/model.js
@@ -0,0 +1,24 @@
+﻿var model = {
+  personer: {
+    velgAlle: false,
+    trekkAntall: 1,
+    liste: [
+      { id: 100, navn: 'Per', erValgt: true },
+      { id: 101, navn: 'Pål', erValgt: true },
+      { id: 102, navn: 'Espen', erValgt: false },
+      { id: 103, navn: 'Ole', erValgt: true },
+    ]
+  },
+  trekninger: [
+    {
+      vinnere: ['Ole'],
+      tid: '2018-10-17 17:10',
+      deltakere: ['Per', 'Pål', 'Ole']
+    }
+    , {
+      vinnere: ['Per', 'Pål', 'Knut'],
+      tid: '2018-10-11 17:10',
+      deltakere: ['Per', 'Pål', 'Ole', 'Knut', 'Gunnar']
+    }
+  ]
+};
